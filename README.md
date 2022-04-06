c200f0f5c72c9357adef5fad9791a9f338811536
Jason Hall <jasonhall@redhat.com>
2022-04-06 13:56:53 -0400

    Support fetching from URLs
    This adds:
    
    - `sget <URL>` to fetch URLs iff a configured trusted identity has
      signed off on the URL's digest in Rekor
    - `sget sign <URL>` to keylessly sign the URL+digest using a Fulcio OIDC
      flow, with the signature appended to Rekor
    - `sget trust` to configure trusted identities
    - `sget <OCI>` to fetch blobs in OCI registries (currently without
      checking signatures)
    
    This also adds documentation to the README.
    
    Signed-off-by: Jason Hall <jasonhall@redhat.com>

--- README.md
+++ README.md
@@ -1,38 +1,88 @@
 # `sget`
 
-`sget` is command for safer, automatic verification of signatures and integration with our binary transparency log, Rekor.
+### 🚨 `sget` is not ready for non-testing use at this time 🚨
+
+`sget` is currently an experiment, and as such is not guaranteed to be safe or reliable.
+
+Its config formats and flags may change in breaking ways at any time.
+Its security properties may change, including in ways that make it _less_ secure than it previously was, or even less secure than _not_ using `sget`. 
+
+Please see the [roadmap](./ROADMAP.md) to get an idea where we're heading.
+
+Join `#sget` on the [Sigstore Slack](https://sigstore.slack.com) ([invite link](https://sigstore.slack.com/join/shared_invite/zt-mhs55zh0-XmY3bcfWn4XEyMqUUutbUQ#/shared-invite/email)) to get involved, ask questions, propose ideas, etc.
+Also feel free to [file an issue](https://github.com/sigstore/sget/issuues/new) to ask a question or report a bug.
+
+With that out of the way...
+
+# What is `sget`?
+
+`sget` is command for safer, automatic verification of signatures and integration with Sigstore's binary transparency log, [Rekor](https://github.com/sigstore/rekor).
+
+It aims to provide a safer alternative to `curl` when fetching artifacts from the internet, and especially when piping those contents to a shell, for example the dreaded and often (correctly!) maligned `curl <url> | sh`.
+
+`curl | sh` isn't a great idea, but `sget | sh` is less-bad.
+
+### Installation
 
 To install `sget`, if you have Go 1.16+, you can directly run:
 
-    $ go install github.com/sigstore/sget@latest
+```
+go install github.com/sigstore/sget@main
+```
 
 and the resulting binary will be placed at `$GOPATH/bin/sget` (or `$GOBIN/sget`, if set).
 
-Just like `curl`, `sget` can be used to fetch artifacts by digest using the OCI URL.
-Digest verification is automatic:
+### `sget <URL>`
 
-```shell
-$ sget us.gcr.io/dlorenc-vmtest2/readme@sha256:4aa3054270f7a70b4528f2064ee90961788e1e1518703592ae4463de3b889dec > artifact
+This will fetch an HTTPS URL and look for any signatures for the contents' digest in the Rekor transparency log.
+
+If any signatures are found matching your trusted identities (see below), the contents will be piped to stdout.
+
+```
+sget https://example.com/archive.zip > archive.zip
 ```
 
-You can also use `sget` to fetch contents by tag.
-Fetching contents without verifying them is dangerous, so we require the artifact be signed in this case:
+or
 
-```shell
-$ sget gcr.io/dlorenc-vmtest2/artifact
-error: public key must be specified when fetching by tag, you must fetch by digest or supply a public key
-
-$ sget --key cosign.pub us.gcr.io/dlorenc-vmtest2/readme > foo
-
-Verification for us.gcr.io/dlorenc-vmtest2/readme --
-The following checks were performed on each of these signatures:
-  - The cosign claims were validated
-  - Existence of the claims in the transparency log was verified offline
-  - The signatures were verified against the specified public key
-  - Any certificates were verified against the Fulcio roots.
+```
+sget https://example.com/install.sh | sh
 ```
 
-The signature, claims and transparency log proofs are all verified automatically by sget as part of the download.
+### `sget sign <URL>`
 
-`curl | bash` isn't a great idea, but `sget | bash` is less-bad.
+This will prompt you to go through the Fulcio OIDC flow to generate a signature for the URL, tied to your identity.
+This record will be appended to the Rekor transparency log so that other users can see it.
+
+If other users configure your identity as a trusted identity, you signature will be accepted when they attempt to fetch the URL using `sget`.
+
+### `sget trust <IDENTITY>...`
+
+This adds the specified identities to your list of trusted identities.
+
+When fetching content using `sget <URL>`, if any of these identities have signatures for the URL in Rekor, the content will be considered to be trusted.
+
+Trusted identities can be removed with the `--rm` flag:
+
+```
+sget trust --rm alice@example.com
+```
+
+Trusted identities can be scoped only to certain URL hostnames with the `--host` flag:
+
+```
+sget trust --host=example.com alice@example.com
+```
+
+In this case, signatures for `alice@example.com` will only be trusted if the URL is being fetched from `example.com`.
+
+### `sget <OCI ref>`
+
+`sget` began its life as a tool to fetch contents stored in an OCI registry, and this functionality remains today.
+
+The main benefit of relying on an OCI registry in this case is that OCI provides a standard and broadly-supported API for fetching content by its digest, i.e., content-addressed storage.
+This means that if someone shares a reference to an object in an OCI registry that includes the digests of its contents, the registry will verify this digest matches before accepting it, and clients (including `sget`) will verify it again before fetching it.
+
+This makes `sget` resistant to attacks where the contents fetched today by one person may differ unexpectedly when fetched tomorrow by someone else.
+
+`sget <OCI ref>` only supports references by digest at this time, and will not currently check for any signatures from trusted identities.
 
--- main.go
+++ main.go
@@ -1,28 +1,225 @@
-//
-// Copyright 2021 The Sigstore Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"io"
 	"log"
+	neturl "net/url"
+	"os"
+	"strings"
 
-	"github.com/sigstore/sget/cli"
+	fapi "github.com/sigstore/fulcio/pkg/api"
+	rekor "github.com/sigstore/rekor/pkg/generated/client"
+	rentries "github.com/sigstore/rekor/pkg/generated/client/entries"
+	rindex "github.com/sigstore/rekor/pkg/generated/client/index"
+	rmodels "github.com/sigstore/rekor/pkg/generated/models"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	if err := cli.New().Execute(); err != nil {
-		log.Fatalf("error during command execution: %v", err)
+	var flags rootFlags
+	root := &cobra.Command{
+		Use:           "sget URL",
+		Args:          cobra.ExactArgs(1),
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			url := args[0]
+			u, err := neturl.Parse(url)
+			if err != nil {
+				return fmt.Errorf("parsing URL: %w", err)
+			}
+			if u.Scheme != "https" {
+				log.Println("URL is not HTTPS, assuming it's an OCI image reference by digest")
+				return fetchImage(url)
+			}
+
+			// Validate digest if specified.
+			tmp, err := fetch(url, false)
+			if err != nil {
+				return fmt.Errorf("error getting digest for %q: %w", url, err)
+			}
+			defer os.Remove(tmp.f.Name())
+			gotDigest := tmp.digest
+			if flags.wantDigest != "" && flags.wantDigest != gotDigest {
+				return fmt.Errorf("digest mismatch; got %q, want %q", gotDigest, flags.wantDigest)
+			}
+
+			// Get Fulcio root cert.
+			fulcioServer, err := neturl.Parse(flags.fulcioURL)
+			if err != nil {
+				return fmt.Errorf("creating Fulcio client: %w", err)
+			}
+			fclient := fapi.NewClient(fulcioServer, fapi.WithTimeout(flags.fulcioTimeout))
+			fresp, err := fclient.RootCert()
+			if err != nil {
+				return fmt.Errorf("getting signing cert: %w", err)
+			}
+			fulcioRoot := x509.NewCertPool()
+			if !fulcioRoot.AppendCertsFromPEM(fresp.ChainPEM) {
+				return fmt.Errorf("failed appending Fulcio root cert")
+			}
+
+			// Find entries for url + digest
+			rclient := rekor.NewHTTPClient(nil)
+			iparams := rindex.NewSearchIndexParams()
+			iparams.SetTimeout(flags.rekorTimeout)
+			iparams.SetQuery(&rmodels.SearchIndex{Hash: "sha256:" + tmp.digest})
+			iresp, err := rclient.Index.SearchIndex(iparams)
+			if err != nil {
+				return fmt.Errorf("querying Rekor entries: %w", err)
+			}
+			if len(iresp.Payload) == 0 {
+				return fmt.Errorf("found no Rekor entries for URL: %s", url)
+			}
+			identities := set{}
+			for _, e := range iresp.Payload {
+				gparams := rentries.NewGetLogEntryByUUIDParams()
+				gparams.SetTimeout(flags.rekorTimeout)
+				gparams.SetEntryUUID(e)
+				gresp, err := rclient.Entries.GetLogEntryByUUID(gparams)
+				if err != nil {
+					return fmt.Errorf("getting Rekor entry %q: %w", e, err)
+				}
+				le := gresp.Payload[e]
+				leb, err := base64.StdEncoding.DecodeString(le.Body.(string))
+				if err != nil {
+					return fmt.Errorf("decoding Rekor LogEntry body: %w", err)
+				}
+				var ent struct {
+					Spec struct {
+						PublicKey []byte
+					}
+				}
+				if err := json.Unmarshal(leb, &ent); err != nil {
+					return fmt.Errorf("unmarshaling Rekor LogEntry body: %w", err)
+				}
+
+				// TODO: Check that the URL matches, not just the digest.
+
+				block, _ := pem.Decode([]byte(ent.Spec.PublicKey))
+				if block == nil {
+					return fmt.Errorf("parsing certificate PEM; block is nil")
+				}
+				cert, err := x509.ParseCertificate(block.Bytes)
+				if err != nil {
+					return fmt.Errorf("parsing certificat: %w", err)
+				}
+
+				// Verify cert is from Fulcio.
+				if _, err := cert.Verify(x509.VerifyOptions{
+					// THIS IS IMPORTANT: WE DO NOT CHECK TIMES HERE
+					// THE CERTIFICATE IS TREATED AS TRUSTED FOREVER
+					// WE CHECK THAT THE SIGNATURES WERE CREATED DURING THIS WINDOW
+					CurrentTime: cert.NotBefore,
+					Roots:       fulcioRoot,
+					KeyUsages: []x509.ExtKeyUsage{
+						x509.ExtKeyUsageCodeSigning,
+					},
+				}); err != nil {
+					return fmt.Errorf("checking cert against Fulcio root: %w", err)
+				}
+
+				if len(cert.EmailAddresses) != 1 {
+					log.Printf("saw unexpected number of identities for %q: %s", e, cert.EmailAddresses)
+				}
+				for _, email := range cert.EmailAddresses {
+					identities.add(email)
+				}
+			}
+
+			// Collect trusted identities.
+			cfg, err := loadConfig()
+			if err != nil {
+				return fmt.Errorf("loading config file: %w", err)
+			}
+			trust := set{}
+			for _, i := range cfg.Identities {
+				trust.add(i)
+			}
+			if h, ok := cfg.Hosts[u.Host]; ok {
+				for _, i := range h.Identities {
+					trust.add(i)
+				}
+			}
+
+			log.Printf("Found %d identities who have signed for %s", len(identities), url)
+			log.Println("Signing identities:", identities) // TODO: remove
+			match := trust.intersect(identities)
+			if len(match) == 0 {
+				return fmt.Errorf("found no trusted identities for %s", url)
+			}
+
+			log.Println("Found trusted identities:", match)
+
+			var outw io.WriteCloser
+			switch flags.out {
+			case "-":
+				outw = os.Stdout
+			default:
+				outw, err = os.Create(flags.out)
+				if err != nil {
+					return fmt.Errorf("creating %q: %w", flags.out, err)
+				}
+			}
+			defer outw.Close()
+
+			// Write the contents of the temp file to stdout.
+			_, err = io.Copy(outw, tmp.out())
+			return err
+		},
 	}
+	flags.addFlags(root)
+
+	addSign(root)
+	addTrust(root)
+
+	if err := root.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type set map[string]struct{}
+
+func (s set) add(n string) { s[n] = struct{}{} }
+
+func (s set) intersect(o set) set {
+	out := set{}
+	for k := range s {
+		if _, ok := o[k]; ok {
+			out[k] = struct{}{}
+		}
+	}
+	return out
+}
+
+func (s set) String() string {
+	var sb strings.Builder
+	first := true
+	for k := range s {
+		if !first {
+			sb.WriteRune(' ')
+		}
+		sb.WriteString(k)
+		first = false
+	}
+	return sb.String()
+}
+
+type rootFlags struct {
+	rekorFlags
+	fulcioFlags
+	wantDigest string
+	out        string
+}
+
+func (f *rootFlags) addFlags(cmd *cobra.Command) {
+	f.rekorFlags.addFlags(cmd)
+	f.fulcioFlags.addFlags(cmd)
+	cmd.Flags().StringVar(&f.wantDigest, "digest", "", "If set, URL must have the given digest")
+	cmd.Flags().StringVarP(&f.out, "out", "o", "-", `File path to write to (for stdout pass "-")`)
 }
